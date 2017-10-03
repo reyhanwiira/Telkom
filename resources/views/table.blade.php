1bf98dc7286f9789ab4d3b525bc8aac1b8014e8b
andikamarsono <32473977+andikamarsono@users.noreply.github.com>
2017-10-03 11:06:50 +0700

    Spongebob
    squarepants

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -185,7 +185,7 @@
 		                 <div class="box-tools pull-right">
 		                   <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 		                 </div>
-                     <td>------------------------</td>
+                     <td>------------------------ kocot</td>
 		               </div><!-- /.box-header -->
 	                <div class="box-body">
 	                  <div class="table-responsive">
