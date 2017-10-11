5b9996130ebf9ec027c1fd5f63c8fbeb995df395
reyhanwiira <reyhanwiira@gmail.com>
2017-10-11 11:03:50 +0700

    edit table dan detail

--- resources/views/layouts/addProject_layout.blade.php
+++ resources/views/layouts/addProject_layout.blade.php
@@ -136,7 +136,6 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-            Add Project
             <small></small>
           </h1>
           <ol class="breadcrumb">
