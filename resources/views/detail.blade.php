f86959448e3b1135555334527ddc606c8fc28d25
reyhanwiira <reyhanwiira@gmail.com>
2017-10-12 09:57:05 +0700

    edit detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -159,242 +159,123 @@
             </div>
             <div id="collapseOne" class="panel-collapse collapse in">
               <div class="box-body">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> cb95c7739bc4d59be4e64e46fc3cf3cdf923a0dc
-=======
-<<<<<<< HEAD
->>>>>>> cb95c7739bc4d59be4e64e46fc3cf3cdf923a0dc
                 <div class="col-md-12">
-=======
-              <div class="col-md-12">
->>>>>>> parent of 99f4a62... UI Revamp
-                  <!-- The time line -->
+                  <div class="col-md-12">
+                    <!-- The time line -->
+                    <ul class="timeline">
+                      <!-- timeline time label -->
+                      <li class="time-label">
+                        <span class="bg-red">
+                          10 Feb. 2014
+                        </span>
+                      </li>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fa fa-user bg-aqua"></i>
+
+                        <div class="timeline-item">
+                          <h3 class="timeline-header no-border"><a href="#">Jay White</a> Rapat dengan client</h3>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fa fa-file-archive-o bg-aqua"></i>
+                        <div class="timeline-item">
+                          <h3 class="timeline-header"><a href="#">Jay White</a> uploaded files</h3>
+                          <div class="timeline-body">
+                            <h4>Hasil Rapat</h4>
+                            <a class="btn btn-app">
+                              <i class="fa fa-save"></i> Save
+                            </a>
+                          </div>
+                          <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline time label -->
+                      <li class="time-label">
+                        <span class="bg-green">
+                          3 Jan. 2014
+                        </span>
+                      </li>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fa fa-camera bg-purple"></i>
+
+                        <div class="timeline-item">
+
+                          <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                          <div class="timeline-body">
+                            <img src="http://placehold.it/150x100" alt="..." class="margin">
+                            <img src="http://placehold.it/150x100" alt="..." class="margin">
+                            <img src="http://placehold.it/150x100" alt="..." class="margin">
+                            <img src="http://placehold.it/150x100" alt="..." class="margin">
+                          </div>
+                          <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+
+                      <li>
+                        <i class="fa fa-clock-o bg-gray"></i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel box box-danger">
+              <div class="box-header with-border">
+                <h4 class="box-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                    Next Action
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="box-body">
                   <ul class="timeline">
                     <!-- timeline time label -->
                     <li class="time-label">
                       <span class="bg-red">
-                        10 Feb. 2014
+                        20 Feb. 2014
                       </span>
                     </li>
                     <!-- /.timeline-label -->
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-envelope bg-blue"></i>
-
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-
-                        <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-
-                        <div class="timeline-body">
-                          Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                          weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                          jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                          quora plaxo ideeli hulu weebly balihoo...
-                        </div>
-                        <div class="timeline-footer">
-                          <a class="btn btn-primary btn-xs">Read more</a>
-                          <a class="btn btn-danger btn-xs">Delete</a>
-                        </div>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
                     <!-- timeline item -->
                     <li>
                       <i class="fa fa-user bg-aqua"></i>
 
                       <div class="timeline-item">
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <h3 class="timeline-header no-border"><a href="#">Jay White</a> Rapat dengan client</h3>
-=======
-=======
->>>>>>> cb95c7739bc4d59be4e64e46fc3cf3cdf923a0dc
-                        <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
-
-                        <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request</h3>
->>>>>>> cb95c7739bc4d59be4e64e46fc3cf3cdf923a0dc
                       </div>
                     </li>
                     <!-- END timeline item -->
                     <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-file-archive-o bg-aqua"></i>
-
-                      <div class="timeline-item">
-
-                        <h3 class="timeline-header"><a href="#">Jay White</a> uploaded files</h3>
-
-                        <div class="timeline-body">
-                          <h4>Hasil Rapat</h4>
-                          <a class="btn btn-app">
-                            <i class="fa fa-save"></i> Save
-                          </a>
-                        </div>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-                    <!-- timeline time label -->
-                    <li class="time-label">
-                      <span class="bg-green">
-                        3 Jan. 2014
-                      </span>
-                    </li>
-                    <!-- /.timeline-label -->
-                    <!-- timeline item -->
-                    <li>
-                      <i class="fa fa-camera bg-purple"></i>
-
-                      <div class="timeline-item">
-                        <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
-
-                        <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-                        <div class="timeline-body">
-                          <img src="http://placehold.it/150x100" alt="..." class="margin">
-                          <img src="http://placehold.it/150x100" alt="..." class="margin">
-                          <img src="http://placehold.it/150x100" alt="..." class="margin">
-                          <img src="http://placehold.it/150x100" alt="..." class="margin">
-                        </div>
-                      </div>
-                    </li>
-                    <!-- END timeline item -->
-                    <!-- timeline item -->
-                    
                     <li>
                       <i class="fa fa-clock-o bg-gray"></i>
                     </li>
+                    <div class="box-tools pull-right">
+                      <a href="" class="btn btn-sm btn-info btn-flat pull-left">Place New Activty</a>
+                    </div><!-- /.box-footer -->
                   </ul>
                 </div>
               </div>
             </div>
           </div>
-          <div class="panel box box-danger">
-            <div class="box-header with-border">
-              <h4 class="box-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                  Next Action
-                </a>
-              </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse">
-              <div class="box-body">
-                <ul class="timeline">
-                  <!-- timeline time label -->
-                  <li class="time-label">
-                    <span class="bg-red">
-                      20 Feb. 2014
-                    </span>
-                  </li>
-                  <!-- /.timeline-label -->
-                  <!-- timeline item -->
-                  <li>
-                    <i class="fa fa-user bg-aqua"></i>
-
-                    <div class="timeline-item">
-                      <h3 class="timeline-header no-border"><a href="#">Jay White</a> Rapat dengan client</h3>
-                    </div>
-                  </li>
-                  <!-- END timeline item -->
-                  <!-- timeline item -->
-                  <li>
-                    <i class="fa fa-clock-o bg-gray"></i>
-                  </li>
-                  <div class="box-tools pull-right">
-                    <a href="{{ url('table/createPro') }}" class="btn btn-sm btn-info btn-flat pull-left">Place New Activty</a>
-                  </div><!-- /.box-footer -->
-                </ul>
-              </div>
-            </div>
-          </div>
         </div>
+        <!-- /.box-body -->
       </div>
-      <!-- /.box-body -->
+      <!-- /.box -->
     </div>
-    <!-- /.box -->
+    <!-- /.col -->
   </div>
-  <!-- /.col -->
-
-  <div class="col-md-6">
-    <div class="box box-solid">
-      <div class="box-header with-border">
-        <h3 class="box-title">Collapsible Accordion</h3>
-      </div>
-      <!-- /.box-header -->
-      <div class="box-body">
-        <div class="box-group" id="accordion">
-          <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-          <div class="panel box box-primary">
-            <div class="box-header with-border">
-              <h4 class="box-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                  Collapsible Group Item #1
-                </a>
-              </h4>
-            </div>
-            <div id="collapseOne" class="panel-collapse collapse in">
-              <div class="box-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-          <div class="panel box box-danger">
-            <div class="box-header with-border">
-              <h4 class="box-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                  Collapsible Group Danger
-                </a>
-              </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse">
-              <div class="box-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-          <div class="panel box box-success">
-            <div class="box-header with-border">
-              <h4 class="box-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                  Collapsible Group Success
-                </a>
-              </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse">
-              <div class="box-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /.box-body -->
-    </div>
-    <!-- /.box -->
-  </div>
-</div>
 </div>
 </section>
 @endsection
