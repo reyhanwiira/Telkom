de7be3409e369021bc1a1148df8c244cdcdcc99d
andikamarsono <32473977+andikamarsono@users.noreply.github.com>
2017-10-16 14:17:01 +0700

    home dan layoutnya di perbaiki lagi
    perbaikan logo taable

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -117,7 +117,7 @@
 
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-pie-chart"></i>
+            <i class="fa fa-table"></i>
             <span>Table</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
