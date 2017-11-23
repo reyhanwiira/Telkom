7e8f5c9dd5371ef74b0df074161199f80a7d97a4
ridgunz <ridwanmhmmd@yahoo.co.id>
2017-11-23 13:16:38 +0700

    fix error redirect login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+      public function logout()
+    {
+        $this->guard()->logout();
+ 
+        return redirect('/login');
+    }
 }
